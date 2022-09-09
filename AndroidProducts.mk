crdroidandroid/android_device_xiaomi_sweet

Initial device tree for sweet
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

AndroidProducts.mk
@@ -1,0 +1,12 @@
+#
+# Copyright (C) 2021 crDroid Android Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_sweet.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_sweet-userdebug \
+    lineage_sweet-eng

BayerischeMotorenWerke/device_xiaomi_sweet-tiramisu

AndroidProducts.mk
@@ -1,0 +1,13 @@
+#
+# Copyright (C) 2021 crDroid Android Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/nad_sweet.mk
+
+COMMON_LUNCH_CHOICES := \
+    nad_sweet-user \
+    nad_sweet-userdebug \
+    nad_sweet-eng