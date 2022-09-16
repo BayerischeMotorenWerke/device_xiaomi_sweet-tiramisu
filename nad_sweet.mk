88d273c7b4719815cba1f6f2dd5af846a6b08812
BayerischeMotorenWerke <syaifulamri199@gmail.com>
2022-09-16 14:52:50 +0700

    sweet: Nusantara Project
    Signed-off-by: BayerischeMotorenWerke <syaifulamri199@gmail.com>

---
+++ nad_sweet.mk
@@ -0,0 +1,30 @@
+# Inherit common products
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit device configurations
+$(call inherit-product, device/xiaomi/sweet/device.mk)
+
+# Inherit common Nusantara configurations
+$(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
+
+# Inherit game space
+$(call inherit-product-if-exists, packages/apps/GameSpace/gamespace.mk)
+NAD_BUILD_TYPE := OFFICIAL
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_GAPPS_ARCH := arm64
+USE_GAPPS := true
+USE_PIXEL_CHARGING := true
+
+PRODUCT_NAME := nad_sweet
+PRODUCT_DEVICE := sweet
+PRODUCT_BRAND := Redmi
+PRODUCT_MODEL := Redmi Note 10 Pro
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="dipper-user 8.1.0 OPM1.171019.011 V9.5.5.0.OEAMIFA release-keys"
+
+BUILD_FINGERPRINT := Xiaomi/dipper/dipper:8.1.0/OPM1.171019.011/V9.5.5.0.OEAMIFA:user/release-keys
